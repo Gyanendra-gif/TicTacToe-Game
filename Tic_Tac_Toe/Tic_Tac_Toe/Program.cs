92b33f3d0e3e8a0c2aa75a17b405ec1592c72bad
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-12-04 11:05:42 +0530

    [Gyanendra] Refactor: A Program to take input from the User return Output

--- Tic_Tac_Toe/Tic_Tac_Toe/Program.cs
+++ Tic_Tac_Toe/Tic_Tac_Toe/Program.cs
@@ -6,11 +6,26 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Tic Tac Game");
-            TicTacToeGame game = new TicTacToeGame();
-            game.CreateGameBoard();
-            game.PlayerInput();
-            Console.WriteLine("Player Position is:" +game.PlayerInput());
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Execute the Program Press- 1-Create Game Board, 2-Player Input, 3-Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                TicTacToeGame game = new TicTacToeGame();
+                switch (choice)
+                {
+                    case 1:
+                        game.CreateGameBoard();
+                        break;
+                    case 2:
+                        game.PlayerInput();
+                        Console.WriteLine("Player Position is:" + game.PlayerInput());
+                        break;
+                    default:
+                         flag = false;
+                        break;
+                }
+            }
         }
     }
 }
