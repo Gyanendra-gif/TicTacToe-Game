72f4a1bc9cc05f1d3999f9c97153ccd7fc1e2cf9
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-12-04 09:32:48 +0530

    [Gyanendra] Add: A Program for Creation of Tic Tac Toe Game Board

---
+++ Tic_Tac_Toe/Tic_Tac_Toe/TicTacToeGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe
+{
+   public class TicTacToeGame
+    {
+        public char[] GAMEBOARD = new char[10];
+        public void CreateGameBoard() 
+        {
+            for (int i = 1; i < GAMEBOARD.Length; i++)
+            {
+                GAMEBOARD[i] = ' ';
+            }   
+        }
+    }
+}
